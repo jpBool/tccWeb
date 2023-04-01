ec7c7c4b280c2e6a1f6fba0e0419cdcadbc4fa21
jpBool <106327899+jpBool@users.noreply.github.com>
2023-04-01 17:59:38 -0300

    Atualizacao 01-04/23
    Database progress

--- tccWeb/app/Http/Controllers/UserController.php
+++ tccWeb/app/Http/Controllers/UserController.php
@@ -10,7 +10,19 @@ class UserController extends Controller
 {
     public function auth(Request $request)
     {
-        return view("loginInicial.login_back");
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+    
+            $stringdeconexao = "host=pgsql.projetoscti.com.br
+                        port=5432
+                        dbname=projetoscti9
+                        user=projetoscti9
+                        password=eq273b294";
+    
+        $conecta = pg_connect($stringdeconexao);
+    
+        $sql = "select * from gp2_usuarios where email = '$email' and senha = '$senha' ";
+        //return view("loginInicial.login_back");
         
         
         //$usuarios = gp2_usuarios::get(); 
--- tccWeb/resources/views/loginInicial/login_back.blade.php
+++ tccWeb/resources/views/loginInicial/login_back.blade.php
@@ -4,8 +4,8 @@
     // script foi chamado de index.php
     // include 'conexao.php';
 
-        $email = $_POST["email"];
-        $senha = $_POST["senha"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
 
         $stringdeconexao = "host=pgsql.projetoscti.com.br
                     port=5432
@@ -13,9 +13,9 @@
                     user=projetoscti9
                     password=eq273b294";
 
-        $conecta = pg_connect($stringdeconexao);
+    $conecta = pg_connect($stringdeconexao);
 
-        $sql = "select * from gp2_usuarios where email = '$email' and senha = '$senha' ";
+    $sql = "select * from gp2_usuarios where email = '$email' and senha = '$senha' ";
     if (pg_num_rows($res) > 0)
     {
          
