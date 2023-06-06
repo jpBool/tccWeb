9bb61dd9ee61367b5e0a2467422fb1a6e6f3c309
BeatrizOsa <sousandcandynive@gmail.com>
2023-06-06 12:18:18 -0300

    t

--- tccWeb/resources/views/loginInicial/login.blade.php
+++ tccWeb/resources/views/loginInicial/login.blade.php
@@ -10,23 +10,6 @@
 
 <body>
     <div class = "main-login">
-        <h1>L O G I N</h1>
-        <form method="post" action="{{route('loginInicial.logar')}}">
-            {{csrf_field() }}
-            <div class="formEmail">
-                <label for="email">Email</label>
-                <input type="text" name="email" placeholder="Email" id="email">
-            </div> 
-            <br>
-            <div class="formSenha">
-                <label for="senha">Senha</label>
-                <input type="password" name="senha" placeholder="Senha" id="senha">
-            </div>
-            <br>
-            <div class="btnLogin">
-                <input type="submit" value="logar">
-            </div>
-        <form>
         <div class="card-login">
             <h1>L O G I N</h1>
             <form method="post" action="{{route('loginInicial.logar')}}">
@@ -41,9 +24,7 @@
                     <input type="password" name="senha" placeholder="Senha" id="senha">
                 </div>
                 <br>
-                <div class="btnLogin">
                     <input type="submit" value="logar" class="botaoLogin">
-                </div>
             <form>
         </div>
         <div>
