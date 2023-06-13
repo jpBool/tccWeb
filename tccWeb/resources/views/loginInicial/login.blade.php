8b802100bfe88758c6f9860914399b914494f446
BeatrizOsa <sousandcandynive@gmail.com>
2023-06-13 11:30:00 -0300

    h

--- tccWeb/resources/views/loginInicial/login.blade.php
+++ tccWeb/resources/views/loginInicial/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <head>
-    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    <link href="{{('assets/css/style.css')}}" rel="stylesheet">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Login</title>
@@ -11,23 +11,29 @@
 <body>
     <div class = "main-login">
         <div class="card-login">
-            <h1>L O G I N</h1>
-            <form method="post" action="{{route('loginInicial.logar')}}">
-                {{csrf_field() }}
-                <div class="formEmail">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" placeholder="Email" id="email">
-                </div> 
-                <br>
-                <div class="formSenha">
-                    <label for="senha">Senha</label>
-                    <input type="password" name="senha" placeholder="Senha" id="senha">
-                </div>
-                <br>
-                    <input type="submit" value="logar" class="botaoLogin">
-            <form>
+            <div class="card-left">
+                <h1>Hello!</h1>
+                <h3>Sign in to your account</h3>
+                <form method="post" action="{{route('loginInicial.logar')}}">
+                    {{csrf_field() }}
+                    <div class="formEmail">
+                        <label for="email">Email</label>
+                        <input type="text" name="email" placeholder="Email" id="email">
+                    </div> 
+                    <br>
+                    <div class="formSenha">
+                        <label for="senha">Senha</label>
+                        <input type="password" name="senha" placeholder="Senha" id="senha">
+                    </div>
+                    <br>
+                        <input type="submit" value="logar" class="botaoLogin">
+                <form>
+            </div>
+            <div class="card-right">
+                <h1>Bem vindo!</h1>
+                <h3>Faça login login para entrar para nosso time</h3>
+            </div>
         </div>
-        <div>
     </div>
 </body>
 </html>          
