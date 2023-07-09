73da6ca3f2eee6fdae2375ef35407a2ba3c4ee77
Marii2305 <mariana.caroline@unesp.br>
2023-07-09 13:51:43 -0300

    sobrecss

--- tccweb2/resources/views/sobre.blade.php
+++ tccweb2/resources/views/sobre.blade.php
@@ -64,6 +64,12 @@
 
     <div class="footer">
         <p class="textfooter"> Sensei </p> 
+        
+            <div class="botoesfooter">
+            <a href="index.blade.php">Home</a>
+            <a href="sobre.blade.php">Sobre</a>
+            <a href="pesquisar.html">Pesquisar</a>
+            </div>
     </div>
 </body>
 </html>
