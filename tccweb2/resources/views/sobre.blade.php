9a5fe205fcfcca19224ddf8638d5823853a95453
Marii2305 <mariana.caroline@unesp.br>
2023-07-22 13:27:21 -0300

    Update sobre.blade.php

--- tccweb2/resources/views/sobre.blade.php
+++ tccweb2/resources/views/sobre.blade.php
@@ -65,8 +65,8 @@
         <p class="textfooter"> Sensei </p> 
         
             <div class="botoesfooter">
-            <a href="index.blade.php">Home</a>
-            <a href="sobre.blade.php">Sobre</a>
+            <a href="index.blade.php">Seguindo</a>
+            <a href="sobre.blade.php">Explorar</a>
             <a href="pesquisar.html">Pesquisar</a>
             </div>
     </div>
