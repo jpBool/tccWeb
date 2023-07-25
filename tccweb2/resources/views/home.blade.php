41317f26d572b2059b429b0031151f1b46902a2e
jpBool <106327899+jpBool@users.noreply.github.com>
2023-07-25 14:23:16 -0300

    a
    a

--- tccweb2/resources/views/home.blade.php
+++ tccweb2/resources/views/home.blade.php
@@ -1,10 +1,21 @@
-@foreach($rows as $row)
+<!DOCTYPE html>
+<html lang="pt-br">
 
+<head>
+    <link href="{{('assets/css/stylehome.css')}}" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Login</title>
+</head>
 
-<div class='project'> 
-<h4>{{$row->nome_projeto}}</h4> --- {{$row->autores}} <br>
-{{$row->email_contato}} <br>
-{{$row->descricao_breve}} 
-</div>
-<br>
-@endforeach
+<body>
+    @foreach($rows as $row)
+    <img src="{{'assets/img_tcc/logo.png'}}" alt="img" id="img-logo">
+    <div class='project'>  
+        <h4>Nova atualização de {{$row->autores}} </h4> --- {{$row->nome_projeto}} <br>
+        {{$row->email_contato}} <br>
+        {{$row->descricao_breve}} 
+    </div>
+    <br>
+    @endforeach  
+</body>
