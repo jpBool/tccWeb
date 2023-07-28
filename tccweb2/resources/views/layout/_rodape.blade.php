b22b369a16ea966893659d4de6b018c01477d303
BeatrizOsa <128929224+BeatrizOsa@users.noreply.github.com>
2023-07-28 02:40:48 -0300

    asas
    asas

--- tccweb2/resources/views/layout/_rodape.blade.php
+++ tccweb2/resources/views/layout/_rodape.blade.php
@@ -1,11 +1,14 @@
 <!--JavaScript at end of body for optimized loading-->
         <div class="footer">
-            <p class="textfooter"> Sensei </p> 
-        
+            <img src="{{'assets/img_tcc/logoSensei.png'}}" alt="img" class="imgfooter">
+
             <div class="botoesfooter">
             <a href="index.blade.php">Home</a>
             <a href="sobre.blade.php">Sobre nós</a>
             <a href="pesquisar.html">Download</a>
+
+            <p class="textfooter"> Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur. </p> 
+        
         </div>
     </div>
     </body>
