690eda637d1885bb7658833840b3e84672b2c67b
Marii2305 <mariana.caroline@unesp.br>
2023-08-07 10:44:32 -0300

    telainicial

--- tccweb2/resources/views/telainicial.blade.php
+++ tccweb2/resources/views/telainicial.blade.php
@@ -6,10 +6,11 @@
     <title>Home</title>
     </head>
     <body>
+
     <div class="header">
                 <nav>  
                     <ul>
-                        <img src="{{'assets/img_tcc/logo.png'}}" alt="img" class="imglogo">
+                        <img src="{{'assets/img_tcc/logo.svg'}}" alt="img" class="imglogo">
                         <div class="navButtom">
                             <li><a class="links" href="index">Seguindo</a></li>
                             <li><a class="links" href="sobre">Explorar</a></li>
@@ -24,32 +25,46 @@
                 </nav>
     </div>
 
+    <div class="globo">                             
+        <img src="{{'assets/img_tcc/globo.png'}}" alt="img" class="imglobo"> 
+    </div>
+
 
         <div class="textos">
+            <br><br><br>
+            
+            <div class="linha-lateral"></div>
 
-             <h1> Bem vindo ao Sensei!</h1><br>
-                <p>Compartilhe seu talento, conecte-se com desenvolvedores e alcance novos patamares.</p><br><br>
-           
 
+            <div class="textprimeiro">
+            <h1> Bem vindo ao Sensei!</h1>
+                <p>Compartilhe seu talento, conecte-se com desenvolvedores e alcance novos patamares.</p>
+            </div>
+
+            <div class="entrar">
             <button type="button" class="button">Criar conta</button>
-            <p class="entrar">Ja possui uma conta? Entrar</p> 
-
-            <div class="text1"> <h2> Compartilhe seus projetos com o mundo</h2><br>
-                <p>Esta plataforma te permite compartilhar projetos com quem desejar, de onde estiver.</p><br><br>
+            <p>Ja possui uma conta? Entrar</p><br>
             </div>
 
-            <div class="text2"> <h2> Procure softwares e encontre desenvolvedores.</h2><br>
-                <p>Apoiando a união de projetos e desenvolvedores de software.</p><br><br><br><br>
-            </div>
+            <div class="linha-lateral1"></div>
 
-            <div class="text3"> <h2> Gerencie seus projetos com a sua equipe.</h2><br>
-                <p>Possibilitando uma melhora profissional, o site possui diversas funcionalidades para sua formação empresa.</p><br><br>
-            </div>
+            <div class="textoscentralizados">
+                <div class="text1"> <h2> Compartilhe seus projetos com o mundo</h2>
+                    <p>Esta plataforma te permite compartilhar projetos com quem desejar, de onde estiver.</p><br><br>
+                </div>
 
-            <div class="text4"> <h2> Navegue em seu feed e descubra novos softwares.</h2><br>
-                <p>As sugestões selecionadas para você, abrirão as portas da exploração e descoberta de suas preferências.</p><br><br>
+                <div class="text2"> <h2> Procure softwares e encontre desenvolvedores.</h2>
+                    <p>Apoiando a união de projetos e desenvolvedores de software.</p><br><br>
+                </div>
+
+                <div class="text3"> <h2> Gerencie seus projetos com a sua equipe.</h2>
+                    <p>Possibilitando uma melhora profissional, o site possui diversas funcionalidades para sua formação empresa.</p><br><br>
+                </div>
+
+                <div class="text4"> <h2> Navegue em seu feed e descubra novos softwares.</h2>
+                    <p>As sugestões selecionadas para você, abrirão as portas da exploração e descoberta de suas preferências.</p><br><br>
+                </div>
             </div>
-        
         </div>
 
 
@@ -65,5 +80,5 @@
         </div>
 
     </div>
-    </body>
+</body>
 </html>
