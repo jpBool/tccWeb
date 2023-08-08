8624eb840d84468018e83d0ba4a767d3daae9a33
kaoriS2 <102991358+kaoriS2@users.noreply.github.com>
2023-08-08 15:25:17 -0300

    tela download inicio

--- tccweb2/resources/views/teladownload.blade.php
+++ tccweb2/resources/views/teladownload.blade.php
@@ -8,18 +8,50 @@
 
 <body class="body">
 
-        <div class="header">
-                <nav>  
-                    <ul>
-                    <img src="{{'assets/img_tcc/logo.png'}}" alt="img" class="imglogo">
-                            <li><a href="index">Seguindo</a></li>
-                            <li><a href="sobre">Explorar</a></li>
-                            <li><a href="pesquisar.html">Pesquisar</a></li>
-                    </ul>
-                </nav>
-        </div>
+    <div class="header">
+        <nav id="navbar">  
+            <ul>
+                <img src="{{'assets/img_tcc/logo.svg'}}" alt="img" class="imglogo">
+
+                <div class="navButtom">
+                    <li><a class="links" href="index">Seguindo</a></li>
+                    <li><a class="links" href="sobre">Explorar</a></li>
+                    <li><a class="links" href="pesquisar.html">Pesquisar</a></li>
+                </div>
+
+                <div class="navIcon">
+                    <img src="{{'assets/img_tcc/perfil.png'}}" alt="img" class="imgperfil">
+                    <img src="{{'assets/img_tcc/confg.png'}}" alt="img" class="imgconfg">
+                </div>
+            </ul>
+        </nav>
+    </div>
 
     <div class="main">
+
+        <div class="Titulo">
+            <h1> Download Sensei</h1>
+        </div>
+
+        <div class="texto"> 
+            <h2> Download Sensei para Windows</h2><br>  
+            <img src="{{'assets/img_tcc/download.png'}}" alt="img" class="imgdownload">
+        </div>
+
+    </div>
+
+    <br><br><br><br><br>
+        
+
+    <div class="footer">
+
+        <img src="{{'assets/img_tcc/logo.png'}}" alt="img" class="imgfooter">
+
+        <div class="botoesfooter">
+        <a href="index.blade.php">Home</a>
+        <a href="sobre.blade.php">Sobre nós</a>
+        <a href="pesquisar.html">Download</a>
+
     </div>
   
 </body>
