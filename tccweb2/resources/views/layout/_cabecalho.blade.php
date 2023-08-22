23d3a3db4bd60c34244aa0d4f4403695aaedb36f
jpBool <106327899+jpBool@users.noreply.github.com>
2023-08-22 11:57:35 -0300

    dfgfdgfdg
    gfgdfg

--- tccweb2/resources/views/layout/_cabecalho.blade.php
+++ tccweb2/resources/views/layout/_cabecalho.blade.php
@@ -8,23 +8,23 @@
     <meta charset="UTF-8">
     <title>Sobre</title>
 
-    <div class="headerA">
-        <nav id="navbar">  
-            <ul>
-                <img src="{{'assets/img_tcc/logo.svg'}}" alt="img" class="imglogo">
-                <div class="navButtom">
-                    <li><a class="links" href="index">Seguindo</a></li>
-                    <li><a class="links" href="sobre">Explorar</a></li>
-                    <li><a class="links" href="{{route('pesquisarAll')}}">Pesquisar</a></li>
-                </div>
+    <div class="header">
+            <nav id="navbar">  
+                <ul>
+                    <img src="{{'assets/img_tcc/logo.svg'}}" alt="img" class="imglogo">
+                    <div class="navButtom">
+                        <li><a class="links" href="index">Seguindo</a></li>
+                        <li><a class="links" href="sobre">Explorar</a></li>
+                        <li><a class="links" href="pesquisar.html">Pesquisar</a></li>
+                    </div>
 
-                <div class="navIcon">
-                    <img src="{{'assets/img_tcc/perfil.png'}}" alt="img" class="imgperfil">
-                    <img src="{{'assets/img_tcc/confg.png'}}" alt="img" class="imgconfg">
-                </div>
-            </ul>
-        </nav>
-    </div>
+                    <div class="navIcon">
+                        <img src="{{'assets/img_tcc/perfil.png'}}" alt="img" class="imgperfil">
+                        <img src="{{'assets/img_tcc/confg.png'}}" alt="img" class="imgconfg">
+                    </div>
+                </ul>
+            </nav>
+        </div> 
         
 </head>
 
--- tccweb2/resources/views/layout/_rodape.blade.php
+++ tccweb2/resources/views/layout/_rodape.blade.php
@@ -1,13 +1,13 @@
 <!--JavaScript at end of body for optimized loading-->
-        <div class="footer">
-            <img src="{{'assets/img_tcc/logoSensei.png'}}" alt="img" class="imgfooter">
+            <div class="footer">
+            <img src="{{'assets/img_tcc/logoSensei.svg'}}" alt="img" class="imgfooter">
 
             <div class="botoesfooter">
                 <a href="index.blade.php">Home</a>
-                <a href="sobre.blade.php">Sobre nós</a>
+                <a href="sobre">Sobre nós</a>
                 <a href="pesquisar.html">Download</a>
+                <p class="emailequipe">sensei.system7@gmail.com</p>
             </div>
-        
-        </div>
+        </div>  
     </body>
   </html>
