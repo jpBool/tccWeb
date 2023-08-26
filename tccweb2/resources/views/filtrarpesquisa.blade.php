e8b2c8b80c40566284a0d129cab5b1e80422cef4
Marii2305 <mariana.caroline@unesp.br>
2023-08-26 17:05:10 -0300

    degr

--- tccweb2/resources/views/filtrarpesquisa.blade.php
+++ tccweb2/resources/views/filtrarpesquisa.blade.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <link href="{{('assets/css/stylesobre.css')}}" rel="stylesheet">
+    <link href="{{('assets/css/stylefiltrar.css')}}" rel="stylesheet">
     <meta charset="UTF-8">
     <title>Sobre</title> 
     </head>
-
-<body class="body">
 
 <div class="header">
         <nav id="navbar">  
@@ -26,46 +24,25 @@
         </nav>
     </div>
 
-    <div class="divtotal1">
-        <div class="text"><h1 class="titulo">About Us</h1>
-            <p>Este sistema visa atender um amplo grupo de pessoas que buscam uma plataforma digital que possa solucionar suas dúvidas no contexto profissional. A nossa prioridade é desenvolver uma plataforma simplificada, garantindo com que seja acessível a todos os públicos e a diversos nichos de programadores e colaboradores empresariais.
-            Através dessa plataforma, busca-se impulsionar a criação de soluções inovadoras, atender necessidades decorrentes, estimular a criatividade e colaboração dos profissionais de programação, além de promover a inclusão digital e democratizar o acesso ao conhecimento tecnológico.</p>
+<body class="body">
+
+<div class="espacinho">
+
+    <div class="separador">
+        <div class="search-container">
+            <input type="text" id="searchInput" placeholder="Pesquisar...">
+            <button id="searchButton"><img class="lupa" src="{{'assets/img_tcc/lupa.svg'}}"></img></button>
         </div>
-    <img src="{{'assets/img_tcc/imgsobre.png'}}" alt="pessoas" class="imgpessoas">
-    </div>
-    
 
-    <div class="devs">
-       
-        <div class="desen"><h2>Desenvolvedores</h2></div>
-       
-            <div class="desen4">
-
-                <div class="bonito"><img src="{{'assets/img_tcc/aline.png'}}" alt="dev1" class="imagem-redonda" >
-                    <div class="caption">Aline Tiemi</div> <div class="caption">aline-tiemi.awoki@unesp.br</div></div>
-                
-                <div class="bonito"><img src="{{'assets/img_tcc/kaori.png'}}" alt="dev2" class="imagem-redonda" >
-                    <div class="caption">Beatriz Kaori</div> <div class="caption">kaori.sakai@unesp.br</div></div>
-                
-                <div class="bonito"><img src="{{'assets/img_tcc/bia.png'}}" alt="dev3" class="imagem-redonda" >
-                    <div class="caption">Beatriz Osajima</div> <div class="caption">beatriz.suwa@unesp.br</div></div>
-
-                <div class="bonito"><img src="{{'assets/img_tcc/joao.png'}}" alt="dev4" class="imagem-redonda" >
-                    <div class="caption">Joâo Pedro</div> <div class="caption">joao-pedro.santos@unesp.br</div></div>
-            </div>
-       
-
-        <div class="desen3">
-            <div class="bonito"><img src="{{'assets/img_tcc/mari.png'}}" alt="dev4" class="imagem-redonda" >
-                <div class="caption">Mariana Caroline</div> <div class="caption">mariana.caroline@unesp.br</div></div>
-
-            <div class="bonito"><img src="{{'assets/img_tcc/matheus.png'}}" alt="dev4" class="imagem-redonda" >
-                <div class="caption">Matheus Gimenes</div> <div class="caption">mathues.gimenes-soares@unesp.br</div></div>
-
-            <div class="bonito"><img src="{{'assets/img_tcc/renan.png'}}" alt="dev4" class="imagem-redonda" >
-                <div class="caption">Renan Hayashi</div> <div class="caption">renan.h.moreira@unesp.br</div></div>
+        <div class="filtrar">
+            <div class="filtro"><img class="filter" src="{{'assets/img_tcc/filtersearch.svg'}}"></img></div>
+            <div class="textfiltro">Filtrar Pesquisa</div>
         </div>
+
     </div>
+
+<div>
+
 
     <div class="footer">
                 <img src="{{'assets/img_tcc/logoSensei.svg'}}" alt="img" class="imgfooter">
