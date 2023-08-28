85bf497addd5185709391f69d5364b97ea95853d
jpBool <106327899+jpBool@users.noreply.github.com>
2023-08-28 10:44:56 -0300

    asdsa
    asdas

--- tccweb2/resources/views/loginInicial/placeholder.blade.php
+++ tccweb2/resources/views/loginInicial/placeholder.blade.php
@@ -14,91 +14,93 @@
 
 <body> 
     <div class="main">
-        <div class="top">
-            <div id="linha-perfil">
-                <div class="top-left">
-                    <div class="img-perfil">
-                        <img src="{{'assets/img_tcc/usuario.png'}}" alt="img" id="img-pess">
-                    </div>
-                    <div class="seguidores-commits">
-                        <div class="seguidores"> 
+        <div class="allall"> 
+            <div class="top">
+                <div id="linha-perfil">
+                    <div class="top-left">
+                        <div class="img-perfil">
+                            <img src="{{'assets/img_tcc/usuario.png'}}" alt="img" id="img-pess">
+                        </div>
+                        <div class="seguidores-commits">
+                            <div class="seguidores"> 
                                 <div class="top-left-left-top-left">
-                                    <labeL>134 seguidores</label>
+                                    <label>134 seguidores</label>
                                 </div>
                                 <div class="top-left-left-top-right">
-                                    <labeL>136 seguindo</label>
+                                    <label>136 seguindo</label>
                                 </div>
-                                </div>
-                                <div id="linha">
-                                </div>
-                                <div class="commits">
-                                    <label>{{$user->commits}}</label>
-                                    <label>commits</label>
-                                </div>
-                                <div class="data-ativo">
-                                    <label>Ativo desde {{$user->inscricao}}</label>
-                                </div>
+                            </div>
+                            <div id="linha">
+                            </div>
+                            <div class="commits">
+                                <label>{{$user->commits}}</label>
+                                <label>commits</label>
+                            </div>
+                            <div class="data-ativo">
+                                <label>Ativo desde {{$user->inscricao}}</label>
+                            </div>
                         </div>
-                </div>
-                <div clss="top-botton">
-                    <label>Nome : {{$user->nome}}</label><br>
-                    <label>Email : {{$user->email}}</label>
-                </div>
-            </div>
-
-            <div class="top-right">
-                <div class="linha-bio">
-                    <div class="top-right-right">
-                        <label>Bio : {{$user->bio}}</label>
+                    </div>
+                    <div class="top-botton">
+                        <label>Nome : {{$user->nome}}</label><br>
+                        <label>Email : {{$user->email}}</label>
                     </div>
                 </div>
-                <button type="button" class="button-perfil">Editar Perfil</button>
+
+                <div class="top-right">
+                    <div class="linha-bio">
+                        <div class="top-right-right">
+                            <label>Bio : {{$user->bio}}</label>
+                        </div>
+                    </div>
+                    <button type="button" class="button-perfil">Editar Perfil</button>
+                </div>
             </div>
             
-        </div>
-
-        <div class="projects-perfil">
-            <label>Seus Projetos: </label>
-            @foreach($rows as $row) 
-                <div class='project'>
-                    <div class="titulo">
-                        <img src="{{'assets/img_tcc/usuario.png'}}" alt="img" id="img-user">
-                        <label class="label-titulo"> Nova atualização de <div id="nome-autor">{{$row->autores}} </div>. Há 18 horas </label>
-                    </div>
-                        
-                    <div class="conteudo"> 
-                        <div>
-                            <img src="{{'assets/img_tcc/gatinho.png'}}" alt="img" id="img-project" width="280px">
-                        </div>
-                        <div class="allConteudo">
-                            <h2> {{$row->nome_projeto}} </h2>
-
-                        <div class="barra-progress">
-                            <div class="progress">Progress</div>
-                            <div class="barraPreta">
-                                <div class="barra" style="width: {{$row->porcentagem}}%;">
-
+            <div class="projects-perfil">
+                <div class="BackProjects">
+                    <label>Seus Projetos: </label> <br><br><br>
+                    @foreach($rows as $row)
+                        <div id="margin-project">
+                            <div class='project'>
+                                <div class="titulo">
+                                    <img src="{{'assets/img_tcc/usuario.png'}}" alt="img" id="img-user">
+                                    <label class="label-titulo"> Nova atualização de <div id="nome-autor">{{$row->autores}} </div>. Há 18 horas </label>
                                 </div>
-                            
+                                    
+                                <div class="conteudo"> 
+                                    <div>
+                                        <img src="{{'assets/img_tcc/gatinho.png'}}" alt="img" id="img-project" width="280px">
+                                    </div>
+                                    <div class="allConteudo">
+                                        <h2> {{$row->nome_projeto}} </h2>
+
+                                        <div class="barra-progress">
+                                            <div class="progress">Progress</div>
+                                            <div class="barraPreta">
+                                                <div class="barra" style="width: {{$row->porcentagem}}%;">
+                                                </div>
+                                            </div>
+                                            <div class="num-porcentagem">{{$row->porcentagem}}%</div>
+                                        </div>
+                                            
+                                        <div class="conteudo-texto">
+                                            {{$row->descricao_breve}} 
+                                            <br>
+                                            <br>
+                                            {{$row->data_criacao}} <br>
+                                            <button type="button" class="button">Show all</button>
+                                        </div> 
+                                    </div> 
+                                </div>
                             </div>
-                            <div class="num-porcentagem">{{$row->porcentagem}}%</div>
                         </div>
-                                
-                            <div class="conteudo-texto">
-                                {{$row->descricao_breve}} 
-                                <br>
-                                <br>
-                                {{$row->data_criacao}} <br>
-                                <button type="button" class="button">Show all</button>
-                            </div> 
-                        </div> 
-                    </div>
-                </div> 
-                @endforeach 
-            <a href="{{route('homeInicial')}}">Home</a>
+                    @endforeach 
+                    <a href="{{route('homeInicial')}}">Home</a>
+                </div>
+            </div>
         </div>
-</div>
+    </div>
+
 </body>
 @endsection 
-
-
