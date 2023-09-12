bf0f233aeabf10fa62045381d3177f4f849391fc
Marii2305 <mariana.caroline@unesp.br>
2023-09-12 10:55:34 -0300

    sobre

--- tccweb2/resources/views/sobre.blade.php
+++ tccweb2/resources/views/sobre.blade.php
@@ -8,22 +8,22 @@
 
 <body class="body">
 
-<div class="header">
-        <nav id="navbar">  
-            <ul>
-                <img src="{{'assets/img_tcc/logo.svg'}}" alt="img" class="imglogo">
-                <div class="navButtom">
-                    <li><a class="links" href="index">Seguindo</a></li>
-                    <li><a class="links" href="sobre">Explorar</a></li>
-                    <li><a class="links" href="filtrarpesquisa">Pesquisar</a></li>
-                </div>
+    <div class="header">
+            <nav id="navbar">  
+                <ul>
+                    <img src="{{'assets/img_tcc/logo.svg'}}" alt="img" class="imglogo">
+                    <div class="navButtom">
+                        <li><a class="links" href="index">Seguindo</a></li>
+                        <li><a class="links" href="sobre">Explorar</a></li>
+                        <li><a class="links" href="pesquisar.html">Pesquisar</a></li>
+                    </div>
 
-                <div class="navIcon">
-                    <img src="{{'assets/img_tcc/perfil.png'}}" alt="img" class="imgperfil">
-                    <img src="{{'assets/img_tcc/confg.png'}}" alt="img" class="imgconfg">
-                </div>
-            </ul>
-        </nav>
+                    <div class="navIcon">
+                        <img src="{{'assets/img_tcc/perfil.png'}}" alt="img" class="imgperfil">
+                        <img src="{{'assets/img_tcc/confg.png'}}" alt="img" class="imgconfg">
+                    </div>
+                </ul>
+            </nav>
     </div>
 
     <div class="divtotal1">
